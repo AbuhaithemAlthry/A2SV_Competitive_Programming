0f5875b86ba4e6161c413247d712a31a2eee7bb3
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-03-24 14:24:31 +0300

    Create Maximum Sum of an Hourglass.py

---
+++ Maximum Sum of an Hourglass.py
@@ -0,0 +1,8 @@
+class Solution:
+    def maxSum(self, grid: List[List[int]]) -> int:
+        sum_ , max_ = 0,0
+        for i in range(len(grid)-2):
+            for j in range(len(grid[0])-2):
+                sum_ = grid[i][j] + grid[i][j+1] + grid[i][j+2] + grid[i+1][j+1] + grid[i+2][j] + grid[i+2][j+1] + grid[i+2][j+2]
+                max_ = max(max_, sum_)
+        return max_

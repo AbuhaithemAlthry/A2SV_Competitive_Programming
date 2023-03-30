ee7e4d6b54151227c7a5c8c55ddbc94af0c158bc
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-03-30 18:22:39 +0300

    Update Hamming Distance.py

--- Hamming Distance.py
+++ Hamming Distance.py
@@ -1,3 +1,7 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
-        return abs(x.bit_length()-y.bit_length())
+        count = 0
+        for i in range(31,-1,-1):
+            if (x & (1 << i)) != (y & (1 << i)) :
+                count+=1
+        return count

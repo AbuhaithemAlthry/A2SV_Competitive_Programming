8a4dacdcc7888ed538c1bdbbce0d92cd2fc2b945
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-04-27 17:49:07 +0300

    Time: 721 ms (42.80%), Space: 23.9 MB (5.56%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,26 +1,26 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
         
-        color = [-1] * (n + 1) # 0 stands for red and 1 stands for blue.
+        color = [-1] * (n + 1) 
         def dfs(node, node_color):
             color[node] = node_color
-            for neighbor in adj[node]:
-                if color[neighbor] == color[node]: return False
+            for neighbor in graph[node]:
+                if color[neighbor] == color[node]: 
+                    return False
                 if color[neighbor] == -1:
                     if not dfs(neighbor, 1 - node_color):
                         return False
             return True
         
-        adj = defaultdict(list)
+        graph = defaultdict(list)
         for dislike in dislikes:
-            adj[dislike[0]].append(dislike[1])
-            adj[dislike[1]].append(dislike[0])
+            graph[dislike[0]].append(dislike[1])
+            graph[dislike[1]].append(dislike[0])
 
         for i in range(1, n + 1):
             if color[i] == -1:
                 # For each pending component, run DFS.
                 if not dfs(i, 1):
-                    print(i,color)
                     # Return false, if there is conflict in the component.
                     return False
         

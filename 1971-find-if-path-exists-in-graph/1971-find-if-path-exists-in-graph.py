207d24d61dc65ab0491b1d5711665a9798f58b36
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-17 18:43:57 +0300

    Time: 3113 ms (5.01%), Space: 152.9 MB (40.37%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.py
@@ -1,20 +1,36 @@
 class Solution:
-    def build(self,edges):
-        graph = defaultdict(list) 
-        for s,d in edges:
-            graph[s].append(d)
-            graph[d].append(s)
-        return graph
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = self.build(edges)
-        def dfs(source, visited):
-            visited.add(source)
-            if source == destination:
-                return True
-            for neighbour in graph[source]:
-                if neighbour not in visited:
-                    found = dfs(neighbour,arr)
-                    if found:
-                        return True
-        arr = set()
-        return dfs(source,arr)
+        if len(edges)==0:
+            return True
+        graph=defaultdict(list)
+        for st,en in edges:
+            graph[st].append(en)
+            graph[en].append(st)
+        
+        def preprocess(city):
+            # components = {}
+            # to avoid if conditions, let’s put all city in their own set
+            for city in graph:
+                components[city] = set([city])
+
+            for city in graph:
+                for neighbor in graph[city]:
+                    if neighbor not in components:
+                        continue
+                    if components[city] is not components[neighbor]:
+                        components[city] |= components[neighbor] # union set
+                        for c in components[neighbor]:
+                            components[c] = components[city] # set references
+            return components
+        
+        def query(component, city1, city2):
+            return city1 in component and city2 in component
+        
+       
+        components = defaultdict(set)
+        for i in range(len(graph)):
+            if i not in components:
+                preprocess(i)
+                if query(components[i], source, destination):
+                    return True
+        return False

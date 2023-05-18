79d677b014ff42f62b998d9aee85614969be2627
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-18 19:47:41 +0300

    Time: 1687 ms (62.32%), Space: 61.1 MB (81.29%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,25 +1,38 @@
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        while x != self.root[x]:
+            x = self.root[x]
+        return x
+		
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
-        graph = defaultdict(list)
+        uf = UnionFind(n+1)
         for st,en,di in roads:
-            graph[st].append(en)
-            graph[en].append(st)
-        def preprocess(city):
-            for city in graph:
-                components[city] = set([city])
-            for city in graph:
-                for neighbor in graph[city]:
-                    if neighbor not in components:
-                        continue
-                    if components[city] is not components[neighbor]:
-                        components[city] |= components[neighbor] # union set
-                        for c in components[neighbor]:
-                            components[c] = components[city] # set references
-            return components
-        components = defaultdict(set)
-        preprocess(1)
-        min_=float(inf)
+            uf.union(st,en)
+            
+        ans = float('inf')
         for st,en,di in roads:
-            if st in components[1] and en in components[1]:
-                min_=min(min_,di)
-        return min_
+            if uf.connected(1,en):
+                ans=min(ans,di)
+
+        return ans
+

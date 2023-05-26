ba4d5f87670c511295dd53a92fd4ed1317453f55
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-26 19:51:25 +0300

    Time: 49 ms (42.67%), Space: 16.3 MB (24.39%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.py
+++ 0509-fibonacci-number/0509-fibonacci-number.py
@@ -6,5 +6,5 @@ class Solution:
         if self.memo[n]:
             return self.memo[n]
         self.memo[n] = self.fib(n-1)+self.fib(n-2)
-        return self.fib(n-1)+self.fib(n-2)
+        return self.memo[n]
         

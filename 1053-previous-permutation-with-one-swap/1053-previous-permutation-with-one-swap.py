7813ec1e84dc6980c7b98087a6bdee35bd4c40cb
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-27 07:28:41 +0300

    Time: 231 ms (74.37%), Space: 17.7 MB (42.71%) - LeetHub

---
+++ 1053-previous-permutation-with-one-swap/1053-previous-permutation-with-one-swap.py
@@ -0,0 +1,13 @@
+class Solution:
+    def prevPermOpt1(self, arr: List[int]) -> List[int]:
+        i = len(arr) - 2
+        while i >= 0 and arr[i] <= arr[i+1]:
+            i -= 1
+        if i >= 0:
+            max_ = i + 1
+			# max number greater on right that less than A[i]
+            for j in range(max_ + 1, len(arr)):
+                if arr[max_] < arr[j] < arr[i]: 
+                    max_ = j
+            arr[max_], arr[i] = arr[i], arr[max_]
+        return arr

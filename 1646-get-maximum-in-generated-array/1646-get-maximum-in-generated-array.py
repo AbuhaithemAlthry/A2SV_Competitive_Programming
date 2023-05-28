7614fd98f3158bf685ff41252ee7cc5411bfe5a6
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-05-28 08:53:46 +0300

    Time: 35 ms (63.51%), Space: 16.5 MB (9.09%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -1,10 +1,19 @@
 class Solution:
     def getMaximumGenerated(self, n: int) -> int:
-        if n==0: return 0
-        nums = [0]*(n+1)
-        nums[1]=1
-        for i in range(1,n//2+1):
-            nums[2 * i] = nums[i]
-            if 2 * i + 1 <= n:
-                nums[2 * i + 1] = nums[i] + nums[i + 1]
-        return max(nums)
+        cache = {}
+
+        def recurse(n):
+            if n < 2:
+                return n
+
+            if n in cache:
+                return cache[n]
+
+            if n % 2 == 0:
+                cache[n] = recurse(n // 2)
+            else:
+                cache[n] = recurse((n - 1) // 2) + recurse((n - 1) // 2 + 1)
+
+            return cache[n]
+
+        return max(recurse(i) for i in range(n + 1))

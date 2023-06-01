c9a7664a149212439b0487a87adee3f6e64dfa66
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-06-01 17:50:22 +0300

    Time: 45 ms (38.61%), Space: 16.3 MB (39.98%) - LeetHub

--- 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
+++ 1646-get-maximum-in-generated-array/1646-get-maximum-in-generated-array.py
@@ -1,19 +1,35 @@
 class Solution:
+#    using top-down
+#     def getMaximumGenerated(self, n: int) -> int:
+#         cache = {}
+
+#         def recurse(n):
+#             if n < 2:
+#                 return n
+
+#             if n in cache:
+#                 return cache[n]
+
+#             if n % 2 == 0:
+#                 cache[n] = recurse(n // 2)
+#             else:
+#                 cache[n] = recurse((n - 1) // 2) + recurse((n - 1) // 2 + 1)
+
+#             return cache[n]
+
+#         return max(recurse(i) for i in range(n + 1))
+
+#   using bottom-up
     def getMaximumGenerated(self, n: int) -> int:
-        cache = {}
-
-        def recurse(n):
-            if n < 2:
-                return n
-
-            if n in cache:
-                return cache[n]
-
-            if n % 2 == 0:
-                cache[n] = recurse(n // 2)
+        if n<2:
+            return n
+        dp = [0]*(n+1)
+        dp[0],dp[1] = 0,1
+        for i in range(2,n+1):
+            if i%2==0:
+                dp[i] = dp[i//2] 
             else:
-                cache[n] = recurse((n - 1) // 2) + recurse((n - 1) // 2 + 1)
-
-            return cache[n]
-
-        return max(recurse(i) for i in range(n + 1))
+                dp[i] = dp[(i-1)//2] + dp[(i-1)//2 +1]
+        return max(dp)
+        
+        

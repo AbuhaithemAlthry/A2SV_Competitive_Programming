29db1259b60372f9cab33333a534beb39541a112
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-06-05 22:40:03 +0300

    Time: 43 ms (56.31%), Space: 16.4 MB (12.48%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,13 +1,23 @@
+# class Solution:
+#     def uniquePaths(self, m: int, n: int) -> int:
+#         cache = {}
+#         def backtrack(y,x):
+#             if (y==m) or (x==n):
+#                 return 1
+#             if (y,x) in cache:
+#                 return cache[(y,x)]
+#             cache[(y,x)] = backtrack(y+1,x) + backtrack(y,x+1)
+            
+#             return cache[(y,x)]
+        
+#         return backtrack(1,1)
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
-        cache = {}
-        def backtrack(y,x):
-            if (y==m) or (x==n):
-                return 1
-            if (y,x) in cache:
-                return cache[(y,x)]
-            cache[(y,x)] = backtrack(y+1,x) + backtrack(y,x+1)
-            
-            return cache[(y,x)]
-        
-        return backtrack(1,1)
+        row = [1]*n
+        matrix = []
+        for i in range(m):
+            matrix.append(row)
+        for i in range(m-2,-1,-1):
+            for j in range(n-2,-1,-1):
+                matrix[i][j]=matrix[i+1][j]+matrix[i][j+1]
+        return matrix[0][0]

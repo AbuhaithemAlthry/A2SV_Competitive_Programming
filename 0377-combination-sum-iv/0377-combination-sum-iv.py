ac95d3af8e0aee60906953482389f528ab3d3d7e
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-06-12 07:28:20 +0300

    Time: 474 ms (5.44%), Space: 18.9 MB (6.95%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.py
+++ 0377-combination-sum-iv/0377-combination-sum-iv.py
@@ -1,14 +1,34 @@
 class Solution:
     def combinationSum4(self, nums: List[int], target: int) -> int:
-        @cache
-        def dfs(target):
-            s = 0
-            if target==0:
-                return 1
-            if target<0:
+        size = len(nums)
+        memo = {}
+        #TopDown approach
+        def dfs(index, current_sum, target, nums, size, memo):
+            if current_sum > target:
                 return 0
-            for child in nums:
-                s += dfs(target-child)
-            return s 
-        
-        return dfs(target)
+
+            if (index,current_sum) in memo:
+                return memo[(index,current_sum)]
+
+            if current_sum == target:
+                return 1
+
+            answer = 0
+            for i in range(size):
+                answer += dfs(i, current_sum + nums[i], target, nums, size, memo)
+
+            memo[(index,current_sum)] = answer
+            return answer
+        return dfs(0, 0, target, nums, size, memo)
+    '''
+    Bottomup approach
+    def combinationSum4(self, N: List[int], T: int) -> int:
+        dp = [0] * (T + 1)
+        dp[0] = 1
+        for i in range(T):
+            if not dp[i]: continue
+            for num in N:
+                if num + i <= T: dp[i+num] += dp[i]
+
+        return dp[T]
+    '''

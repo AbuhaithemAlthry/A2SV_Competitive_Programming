93bb6ccfc084bc0d8d18f0285aa20ea2ba1151f4
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-07-15 16:51:57 +0300

    Time: 39 ms (92.64%), Space: 16.4 MB (56.53%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,23 +1,26 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        if not digits:
+        if digits == "":
             return []
-        dictionary = {
-            "2": ["a", "b", "c"],
-            "3": ["d", "e", "f"],
-            "4": ["g", "h", "i"],
-            "5": ["j", "k", "l"],
-            "6": ["m", "n", "o"],
-            "7": ["p", "q", "r", "s"],
-            "8": ["t", "u", "v"],
-            "9": ["w", "x", "y", "z"],
-        }
+        n = len(digits)
+        res = []
+        dictionary = {"2":["a","b","c"],
+              "3":["d","e","f"],
+              "4":["g","h","i"],
+              "5":["j","k","l"],
+              "6":["m","n","o"],
+              "7":["p","q","r","s"],
+              "8":["t","u","v"],
+              "9":["w","x","y","z"]}
+        
+        def backtrack(res,subset, index,digits):
+            if len(digits) == index:
+                res.append(''.join(subset))
+                return 
+            for i in dictionary[digits[index]]:
+                subset.append(i)
+                backtrack(res,subset,index+1,digits)
+                subset.pop()
 
-        dp = [""]
-        for digit in digits:
-            current_combinations = []
-            for existing_comb in dp:
-                for new_char in dictionary[digit]:
-                    current_combinations.append(existing_comb + new_char)
-            dp = current_combinations
-        return dp
+        backtrack(res,[],0,digits)
+        return res

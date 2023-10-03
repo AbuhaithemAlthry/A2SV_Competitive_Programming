a16f776c4bd778377ecd0c933829612be82a6188
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-10-03 14:58:40 +0300

    Time: 1992 ms (19.04%), Space: 34.7 MB (5.23%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divroblox-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div792-number-of-matching-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divroblox-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div792-number-of-matching-subsequences.py
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divroblox-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div792-number-of-matching-subsequences/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divgoogle-divdiv-classcompanytagscontainer-tagoccurence15-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divroblox-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div792-number-of-matching-subsequences.py
@@ -1,16 +1,36 @@
+class Node:
+    def __init__(self):
+        self.isTerminal = 0
+        self.children = [None] * 26
+
+class Trie:
+    def __init__(self):
+        self.root = Node()
+
+    def insert(self, s):
+        cur = self.root
+        for ch in s:
+            if not cur.children[ord(ch) - ord('a')]:
+                cur.children[ord(ch) - ord('a')] = Node()
+            cur = cur.children[ord(ch) - ord('a')]
+        cur.isTerminal += 1
+
+    def dfs(self, root, s, idx, pos):
+        res = 0
+        for i in range(26):
+            if root.children[i]:
+                newIdx = bisect.bisect(pos[i], idx)
+                if newIdx == len(pos[i]):
+                    continue
+                res += self.dfs(root.children[i], s, pos[i][newIdx], pos)
+        return res + root.isTerminal
+
 class Solution:
-    def numMatchingSubseq(self, s: str, words: List[str]) -> int:
-        sd = defaultdict(list)
-        for i,c in enumerate(s):
-            sd[c].append(i)
-        
-        ct = 0
-        for w in words:            
-            pre = -1
-            for c in w:
-                pos = bisect.bisect(sd[c], pre)
-                if pos==len(sd[c]): break
-                pre = sd[c][pos]
-            else:ct+=1
-        return ct
-                
+    def numMatchingSubseq(self, s, words):
+        t = Trie()
+        for word in words:
+            t.insert(word)
+        pos = [[] for _ in range(26)]
+        for i, ch in enumerate(s):
+            pos[ord(ch) - ord('a')].append(i)
+        return t.dfs(t.root, s, -1, pos)

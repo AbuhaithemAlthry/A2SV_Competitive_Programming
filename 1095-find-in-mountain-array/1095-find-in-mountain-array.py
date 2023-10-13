705eb1926cd07bbd44e6af9f7c1c6c62f48035f2
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-10-13 14:13:12 +0300

    Time: 30 ms (95.43%), Space: 17.1 MB (90.09%) - LeetHub

--- 1095-find-in-mountain-array/1095-find-in-mountain-array.py
+++ 1095-find-in-mountain-array/1095-find-in-mountain-array.py
@@ -1,41 +1,55 @@
 class Solution:
     def findInMountainArray(self, target: int, mountain_arr: 'MountainArray') -> int:
-        length = mountain_arr.length()
-
-        low = 1
-        high = length - 2
-        while low != high:
-            test_index = (low + high) // 2
-            if mountain_arr.get(test_index) < mountain_arr.get(test_index + 1):
-                low = test_index + 1
-            else:
-                high = test_index
-        peak_index = low
-
-
-        low = 0
-        high = peak_index
-        while low != high:
-            test_index = (low + high) // 2
-            if mountain_arr.get(test_index) < target:
-                low = test_index + 1
-            else:
-                high = test_index  
+        left,right = -1, mountain_arr.length()
+        N = mountain_arr.length()
+        while right > left + 1:
+            md = left + (right - left) // 2
+            currValue = mountain_arr.get(md)
+            nxtValue = mountain_arr.get(md + 1) if md + 1 < N else float("-inf")
+            
+            if currValue >= nxtValue:
+                right = md
                 
-        if mountain_arr.get(low) == target:
-            return low
-        
-
-        low = peak_index + 1
-        high = length - 1
-        while low != high:
-            test_index = (low + high) // 2
-            if mountain_arr.get(test_index) > target:
-                low = test_index + 1
             else:
-                high = test_index
-                
-        if mountain_arr.get(low) == target:
-            return low
+                left = md
+         
+        peak = right
+ 
+        def binRight(left,right):
+            while right > left + 1:
+                md = left + (right - left) // 2
+                currValue = mountain_arr.get(md)
+
+                if currValue <= target:
+                    right = md 
+
+                else:
+                    left = md 
         
-        return -1
+            return right
+        
+        def binLeft(left,right):
+            while right > left + 1:
+                md = left + (right - left) // 2
+                currValue = mountain_arr.get(md)
+
+                if currValue >= target:
+                    right = md
+
+                else:
+                    left = md
+                    
+            return right
+
+        leftRes = binLeft(-1,peak + 1)
+        rightRes = binRight(peak - 1,N)
+
+        ans = -1
+        if rightRes < N:
+            ans = rightRes if mountain_arr.get(rightRes) == target else ans
+        
+        if leftRes >= 0:
+            ans = leftRes if mountain_arr.get(leftRes) == target else ans
+    
+        return ans
+        

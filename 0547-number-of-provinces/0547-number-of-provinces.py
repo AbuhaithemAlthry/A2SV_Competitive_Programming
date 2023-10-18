705417141bfa3399f017c4e72f57765bd1304283
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-10-18 11:23:12 +0300

    Time: 226 ms (16.73%), Space: 17 MB (89.81%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,19 +1,43 @@
+from collections import defaultdict
+from typing import List
+
+# UnionFind class
+class UnionFind:
+    def __init__(self, size):
+        self.root = [i for i in range(size)]
+        self.rank = [1] * size
+
+    def find(self, x):
+        while x != self.root[x]:
+            x = self.root[x]
+        return x
+
+    def union(self, x, y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+        if rootX != rootY:
+            if self.rank[rootX] > self.rank[rootY]:
+                self.root[rootY] = rootX
+            elif self.rank[rootX] < self.rank[rootY]:
+                self.root[rootX] = rootY
+            else:
+                self.root[rootY] = rootX
+                self.rank[rootX] += 1
+
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        n = len(isConnected)
-        numberOfComponents = 0
-        visit = [False] * n
-        
-        def dfs(node: int, isConnected: List[List[int]], visit: List[bool]) -> None:
-            visit[node] = True
-            for i in range(len(isConnected)):
-                if isConnected[node][i] and not visit[i]:
-                    dfs(i, isConnected, visit)
+        uf = UnionFind(len(isConnected))
 
-        for i in range(n):
-            if not visit[i]:
-                numberOfComponents += 1
-                dfs(i, isConnected, visit)
+        for i in range(len(isConnected)):
+            for j in range(len(isConnected[i])):
+                if isConnected[i][j] == 1:  # Check for a connected province
+                    uf.union(i, j)
 
+        dic = defaultdict(int)
+        for i in range(len(isConnected)):
+            dic[uf.find(i)] += 1
 
-        return numberOfComponents
+        return len(dic)

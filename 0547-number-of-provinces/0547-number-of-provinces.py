a775d2d37c1cd973093c8b39fbd6b1b5da4340d4
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-10-18 11:38:02 +0300

    Time: 212 ms (42.16%), Space: 16.8 MB (99.90%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,28 +1,18 @@
-from collections import defaultdict
-from typing import List
-
-# UnionFind class
 class UnionFind:
     def __init__(self, size):
         self.root = [i for i in range(size)]
-        self.rank = [1] * size
 
     def find(self, x):
-        while x != self.root[x]:
-            x = self.root[x]
-        return x
-
+        if x == self.root[x]:
+            return x
+        self.root[x] = self.find(self.root[x])
+        return self.root[x]
+		
     def union(self, x, y):
         rootX = self.find(x)
         rootY = self.find(y)
         if rootX != rootY:
-            if self.rank[rootX] > self.rank[rootY]:
-                self.root[rootY] = rootX
-            elif self.rank[rootX] < self.rank[rootY]:
-                self.root[rootX] = rootY
-            else:
-                self.root[rootY] = rootX
-                self.rank[rootX] += 1
+            self.root[rootY] = rootX
 
     def connected(self, x, y):
         return self.find(x) == self.find(y)
@@ -32,12 +22,11 @@ class Solution:
         uf = UnionFind(len(isConnected))
 
         for i in range(len(isConnected)):
-            for j in range(len(isConnected[i])):
-                if isConnected[i][j] == 1:  # Check for a connected province
-                    uf.union(i, j)
+            for j in range(len(isConnected[0])):
+                if isConnected[i][j]:
+                    uf.union(i,j)
 
         dic = defaultdict(int)
         for i in range(len(isConnected)):
             dic[uf.find(i)] += 1
-
         return len(dic)

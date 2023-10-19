9b9afc05750b50e47cacb686c6699836867a8480
SEID MUHAMMED <abuhaithemalthry@gmail.com>
2023-10-19 18:16:29 +0300

    Time: 40 ms (64.92%), Space: 16.1 MB (99.51%) - LeetHub

---
+++ 0767-reorganize-string/0767-reorganize-string.py
@@ -0,0 +1,22 @@
+class Solution:
+    def reorganizeString(self, s: str) -> str:
+        counter = Counter(s)
+        store = [(-j, i) for i, j in counter.items()]
+        heapify(store)
+        res = ""
+        while len(store) > 1:
+            # print(store)
+            x = heappop(store)
+            y = heappop(store)
+           
+            res += x[1] + y[1]
+            if x[0] < (-1):
+                heappush(store, (x[0]+1, x[1]))
+            if y[0] < (-1):
+                heappush(store, (y[0]+1, y[1]))
+        # print()
+        if store and store[0][0] < -1:
+            return ""
+
+
+        return res + store[0][1] if store else res
